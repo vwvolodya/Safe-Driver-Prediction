17b81a555a5c4682111e2f207b5662ac4068ee28
Holubets Volodymyr <vwvolodya@gmail.com>
2017-10-08 10:58:22 +0300

    added data

--- solution/inference.py
+++ solution/inference.py
@@ -1,6 +1,13 @@
-from solution.model import DriverClassifier, DriverDataset, ToTensor
+from solution.dataset import DriverDataset, ToTensor
+from solution.model import DriverClassifier
+from torch.utils.data import DataLoader
 
 
-test_dataset = DriverDataset("../data/for_train.csv", transform=ToTensor())
-net = DriverClassifier.load("models/model_1.mdl")
+test_dataset = DriverDataset("../data/for_test.csv", transform=ToTensor())
+net = DriverClassifier.load("../models/model_1.mdl")
 
+dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=1)
+
+res = net.evaluate(dataloader)
+
+print(res)
